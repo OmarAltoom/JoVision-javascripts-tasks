dea79d65675befe0fe65cd426207d810fd009dd5
OmarAltoom <toom81138@gmail.com>
2025-02-02 23:48:39 +0300

    task8

---
+++ task8/task8.js
@@ -0,0 +1,7 @@
+document.getElementById("b").addEventListener("click",req);
+function req(){
+    let fetchh = fetch("https://api.ipify.org/");
+    fetchh.then(d => {
+        console.log(d)
+    });
+}
